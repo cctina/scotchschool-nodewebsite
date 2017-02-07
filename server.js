0544ca29854adfad9586e9eca40a88532842e70b
Yu-Chen Hsu <every2ever@gmail.com>
2017-02-07 01:37:39 -0800

    Use module express

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
-// Import http module
-const http = require('http');
+// // Import http module
+// const http = require('http');
 
-// handle requests/responses
-var handleRequest = function (request, response) {
-	// Return a string
-	response.end('Hello world!');
-};
+// // handle requests/responses
+// var handleRequest = function (request, response) {
+// 	// Return a string
+// 	response.end('Hello world!');
+// };
 
-// Create http server
-const server = http.createServer(handleRequest);
+// // Create http server
+// const server = http.createServer(handleRequest);
 
-// Start server & listen to port xxx
-server.listen(8080, function () {
-	console.log('Listen to port 8080');
+// // Start server & listen to port xxx
+// server.listen(8080, function () {
+// 	console.log('Listen to port 8080');
+// });
+
+//////////////////// EXPRESS /////////////////////////
+
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+// Start the server 
+app.listen(PORT, function () {
+	console.log('app started');
 });
+
+// Route our app
+app.get('/', function (req, res) {
+	res.send('Hello world!');
+})
